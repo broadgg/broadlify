074258f8f2a6fa239f966783a9909dadf980783f
Marek Vargovčík <vargovcik.marek@gmail.com>
2022-05-11 07:27:30 +0200

    added cdk deployment of static website

--- bin/infrastructure.ts
+++ bin/infrastructure.ts
@@ -1,22 +1,24 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
 
-import { BroadlifyStack } from '../lib/infrastructure';
+import * as cdk from 'aws-cdk-lib';
+import { Infrastructure } from '../lib/infrastructure';
+
+class InfrastructureStack extends cdk.Stack {
+    constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
+        super(parent, name, props);
+
+        new Infrastructure(this, 'Infrastructure');
+    }
+}
 
 const app = new cdk.App();
-new BroadlifyStack(app, 'BroadlifyStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new InfrastructureStack(app, 'InfrastructureStack', {
+    env: {
+        account: app.node.tryGetContext('accountId'),
+        region: 'us-east-1',
+    }
 });
+
+app.synth();
--- lib/infrastructure.ts
+++ lib/infrastructure.ts
@@ -1,16 +1,74 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Duration, RemovalPolicy } from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class BroadlifyStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+const DOMAIN_NAME = 'marekvargovcik.com';
+export class Infrastructure extends Construct {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
 
-    // The code that defines your stack goes here
+    const zone = route53.HostedZone.fromLookup(this, 'Zone', { domainName: DOMAIN_NAME });
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, 'CloudfrontOAI');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BroadlifyQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      bucketName: DOMAIN_NAME,
+      publicReadAccess: false,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    siteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [siteBucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
+
+    const certificate = new acm.DnsValidatedCertificate(this, 'SiteCertificate', {
+      domainName: DOMAIN_NAME,
+      hostedZone: zone,
+      region: 'us-east-1',
+    });
+
+    const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
+      certificate: certificate,
+      defaultRootObject: "index.html",
+      domainNames: [DOMAIN_NAME],
+      minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      errorResponses:[
+        {
+          httpStatus: 403,
+          responseHttpStatus: 403,
+          responsePagePath: '/error.html',
+          ttl: Duration.minutes(30),
+        }
+      ],
+      defaultBehavior: {
+        origin: new cloudfrontOrigins.S3Origin(siteBucket, {originAccessIdentity: cloudfrontOAI}),
+        compress: true,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      }
+    })
+
+    new route53.ARecord(this, 'SiteAliasRecord', {
+      recordName: DOMAIN_NAME,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+      zone
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
+      sources: [s3deploy.Source.asset('./web')],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
   }
 }
