3558183fcaf49298d1d24e9da2c29fd1470e7497
michal-cic <michal.cic@gmail.com>
2022-05-16 11:40:59 +0200

    directus backend refactoring

--- bin/infrastructure.ts
+++ bin/infrastructure.ts
@@ -3,15 +3,14 @@ import "source-map-support/register";
 
 import * as cdk from "aws-cdk-lib";
 import { Infrastructure } from "../lib/infrastructure";
-import { Iam } from "../lib/directus/iam";
-import { DirectusStorage } from "../lib/directus/storage";
-import { DirectusApp } from "../lib/directus/directus";
+import { Backend } from "../lib/backend";
 
 class InfrastructureStack extends cdk.Stack {
   constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
     super(parent, name, props);
 
     new Infrastructure(this, "Infrastructure");
+    new Backend(this, "Backend");
   }
 }
 
@@ -22,22 +21,6 @@ new InfrastructureStack(app, "InfrastructureStack", {
     account: app.node.tryGetContext("accountId"),
     region: "us-east-1",
   },
-});
-
-const APP_NAME = "test";
-
-const iam = new Iam(app, "DirectusIAM", { appName: APP_NAME });
-const storage = new DirectusStorage(app, "DirectusStorage", {
-  appName: APP_NAME,
-});
-
-storage.bucket.grantReadWrite(iam.role);
-storage.bucket.grantDelete(iam.role);
-
-const directusApp = new DirectusApp(app, "DirectusApp", {
-  appName: APP_NAME,
-  fileBucket: storage.bucket,
-  iam,
 });
 
 app.synth();
