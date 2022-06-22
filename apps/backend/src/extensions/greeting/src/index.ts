2c5f5a79068897031a3b5291dbcbc1d5d2bff501
Marek Vargovčík <vargovcik.marek@gmail.com>
2022-06-22 15:51:12 +0200

    corrected url for greetings extension

--- apps/backend/src/extensions/greeting/src/index.ts
+++ apps/backend/src/extensions/greeting/src/index.ts
@@ -1,5 +1,5 @@
 import { defineEndpoint } from '@directus/extensions-sdk';
 
 export default defineEndpoint((router) => {
-  router.get('/', (_req, res) => res.send('Hello, World!'));
+  router.get('/greetings', (_req, res) => res.send('Hello, World!'));
 });
